6df09acc19faeb41397f66179c2ecdd8e774fc50
amber1093 <ozan032@gmail.com>
2024-02-25 04:08:11 +0300

    add template project

---
+++ src/main/java/amber1093/respite_bench/RespiteBenchDataGenerator.java
@@ -0,0 +1,11 @@
+package amber1093.respite_bench;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RespiteBenchDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
