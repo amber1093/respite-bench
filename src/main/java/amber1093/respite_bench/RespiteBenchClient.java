af6973a5197076c7f04d7c9c421c3937aac331ae
amber1093 <ozan032@gmail.com>
2024-02-25 22:29:32 +0300

    Add description and license

---
+++ src/main/java/amber1093/respite_bench/RespiteBenchClient.java
@@ -0,0 +1,13 @@
+package amber1093.respite_bench;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RespiteBenchClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    }
+
+}
