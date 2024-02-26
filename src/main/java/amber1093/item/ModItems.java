b6e506acdde536b6500b5f6cece2ecd3f96cb4c0
amber1093 <ozan032@gmail.com>
2024-02-26 18:24:52 +0300

    Add flasks to item registry

---
+++ src/main/java/amber1093/item/ModItems.java
@@ -0,0 +1,33 @@
+package amber1093.item;
+
+import amber1093.respite_bench.RespiteBench;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item FLASK = registerItem("flask", new Item(new FabricItemSettings()));
+    public static final Item EMPTY_FLASK = registerItem("empty_flask", new Item(new FabricItemSettings()));
+
+    private static void addItemsToFoodAndDrinkItemGroup(FabricItemGroupEntries entries) {
+        entries.add(FLASK);
+        entries.add(EMPTY_FLASK);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(RespiteBench.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        RespiteBench.LOGGER.info("Registering items for " + RespiteBench.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToFoodAndDrinkItemGroup);
+    }
+
+}
--- src/main/java/amber1093/respite_bench/RespiteBench.java
+++ src/main/java/amber1093/respite_bench/RespiteBench.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import amber1093.item.ModItems;
+
 public class RespiteBench implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -17,6 +19,6 @@ public class RespiteBench implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
