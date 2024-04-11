385801da63618aa9380d89fe1da5957fb31f7cb5
amber1093 <ozan032@gmail.com>
2024-04-12 00:42:32 +0300

    Add tooltips to MobRespawnerScreen

--- src/main/java/amber1093/respite_bench/screen/MobRespawnerScreen.java
+++ src/main/java/amber1093/respite_bench/screen/MobRespawnerScreen.java
@@ -7,10 +7,13 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
@@ -22,8 +25,11 @@ public class MobRespawnerScreen extends Screen {
 	private static final int BACKGROUND_HEIGHT = 174;
 	private static final int TEXTFIELD_WIDGET_WIDTH = 26;
 	private static final int BUTTON_WIDGET_WIDTH = 96;
+	private static final int SHORT_TEXT_WIDGET_WIDTH = 96;
+	private static final int LONG_TEXT_WIDGET_WIDTH = 150;
 	private static final int WIDGET_HEIGHT = 16;
 	private static final int EDGE_SPACING = 8;
+	private static final int WIDGET_SPACING = 19;
 
 	public BlockPos blockPos;
 	public int maxConnectedEntities;
@@ -37,10 +43,6 @@ public class MobRespawnerScreen extends Screen {
 	public TextFieldWidget spawnCountWidget;
 	public TextFieldWidget requiredPlayerRangeWidget;
 	public TextFieldWidget spawnRangeWidget;
-	public ButtonWidget entityDataClearWidget;
-	public ButtonWidget connectedEntitiesKillAllWidget;
-	public ButtonWidget doneButtonWidget;
-	
 
 	public MobRespawnerScreen(Text title, BlockPos pos, int maxConnectedEntities, int spawnCount, int requiredPlayerRange, int spawnRange) {
 		super(title);
@@ -53,41 +55,43 @@ public class MobRespawnerScreen extends Screen {
 
 	@Override
 	protected void init() {
+		int leftEdge = getLeftEdge(width);
 		int rightEdge = getRightEdge(width);
 		int topEdge = getTopEdge(height);
 		
 
-		//#region widget constructors
+		//#region textfield constructors
 		maxConnectedEntitiesWidget = new TextFieldWidget(
 			this.textRenderer,
-			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + 20,
-			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT, ScreenTexts.EMPTY
+			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + WIDGET_SPACING,
+			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT,
+			Text.literal(String.valueOf(this.maxConnectedEntities))
 		);
 
 		spawnCountWidget = new TextFieldWidget(
 			this.textRenderer,
-			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + 40,
-			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT, ScreenTexts.EMPTY
+			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + (WIDGET_SPACING * 2),
+			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT,
+			Text.literal(String.valueOf(this.spawnCount))
 		);
 
 		requiredPlayerRangeWidget = new TextFieldWidget(
 			this.textRenderer,
-			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + 60,
-			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT, ScreenTexts.EMPTY
+			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + (WIDGET_SPACING * 3),
+			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT,
+			Text.literal(String.valueOf(this.requiredPlayerRange))
 		);
 
 		spawnRangeWidget = new TextFieldWidget(
 			this.textRenderer,
-			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + 80,
-			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT, ScreenTexts.EMPTY
+			rightEdge - TEXTFIELD_WIDGET_WIDTH, topEdge + (WIDGET_SPACING * 4),
+			TEXTFIELD_WIDGET_WIDTH, WIDGET_HEIGHT,
+			Text.literal(String.valueOf(this.spawnRange))
 		);
+		//#endregion
 
-		maxConnectedEntitiesWidget.setText(String.valueOf(this.maxConnectedEntities));
-		spawnCountWidget.setText(String.valueOf(this.spawnCount));
-		requiredPlayerRangeWidget.setText(String.valueOf(this.requiredPlayerRange));
-		spawnRangeWidget.setText(String.valueOf(this.spawnRange));
-
-		entityDataClearWidget = (
+		//#region button constructors
+		ButtonWidget entityDataClearWidget = (
 			ButtonWidget.builder(
 				Text.translatable("screen.respite_bench.mob_respawner.entitydata.clear"),
 				button -> {
@@ -95,11 +99,11 @@ public class MobRespawnerScreen extends Screen {
 					close();
 				}
 			)
-			.dimensions(rightEdge - BUTTON_WIDGET_WIDTH, topEdge + 100, BUTTON_WIDGET_WIDTH, WIDGET_HEIGHT)
+			.dimensions(rightEdge - BUTTON_WIDGET_WIDTH, topEdge + (WIDGET_SPACING * 5), BUTTON_WIDGET_WIDTH, WIDGET_HEIGHT)
 			.build()
 		);
 
-		connectedEntitiesKillAllWidget = (
+		ButtonWidget connectedEntitiesKillAllWidget = (
 			ButtonWidget.builder(
 				Text.translatable("screen.respite_bench.mob_respawner.connectedentitiesuuid.disconnectall"),
 				button -> {
@@ -107,11 +111,11 @@ public class MobRespawnerScreen extends Screen {
 					close();
 				}
 			)
-			.dimensions(rightEdge - BUTTON_WIDGET_WIDTH, topEdge + 120, BUTTON_WIDGET_WIDTH, WIDGET_HEIGHT)
+			.dimensions(rightEdge - BUTTON_WIDGET_WIDTH, topEdge + (WIDGET_SPACING * 6), BUTTON_WIDGET_WIDTH, WIDGET_HEIGHT)
 			.build()
 		);
 
-		doneButtonWidget = (
+		ButtonWidget doneButtonWidget = (
 			ButtonWidget.builder(
 				ScreenTexts.DONE,
 				button -> {
@@ -123,14 +127,74 @@ public class MobRespawnerScreen extends Screen {
 		);
 		//#endregion
 
+		//#region text constructors
+		TextWidget maxConnectedEntitiesTextWidget = new TextWidget(
+			leftEdge, topEdge + WIDGET_SPACING,
+			LONG_TEXT_WIDGET_WIDTH, WIDGET_HEIGHT,
+			ScreenTexts.EMPTY, this.textRenderer
+		);
+		maxConnectedEntitiesTextWidget.alignLeft();
+		maxConnectedEntitiesTextWidget.setTooltip(getTextWidgetTooltip("maxconnectedentities", false));	
+		
+		TextWidget spawnCountTextWidget = new TextWidget(
+			leftEdge, topEdge + (WIDGET_SPACING * 2),
+			LONG_TEXT_WIDGET_WIDTH, WIDGET_HEIGHT,
+			ScreenTexts.EMPTY, this.textRenderer
+		);
+		spawnCountTextWidget.alignLeft();
+		spawnCountTextWidget.setTooltip(getTextWidgetTooltip("spawncount", true));	
+		
+		TextWidget requiredPlayerRangeTextWidget = new TextWidget(
+			leftEdge, topEdge + (WIDGET_SPACING * 3),
+			LONG_TEXT_WIDGET_WIDTH, WIDGET_HEIGHT,
+			ScreenTexts.EMPTY, this.textRenderer
+		);
+		requiredPlayerRangeTextWidget.alignLeft();
+		requiredPlayerRangeTextWidget.setTooltip(getTextWidgetTooltip("requiredplayerrange", true));	
+		
+		TextWidget spawnRangeTextWidget = new TextWidget(
+			leftEdge, topEdge + (WIDGET_SPACING * 4),
+			LONG_TEXT_WIDGET_WIDTH, WIDGET_HEIGHT,
+			ScreenTexts.EMPTY, this.textRenderer
+		);
+		spawnRangeTextWidget.alignLeft();
+		spawnRangeTextWidget.setTooltip(getTextWidgetTooltip("spawnrange", true));	
+
+		TextWidget entityDataTextWidget = new TextWidget(
+			leftEdge, topEdge + (WIDGET_SPACING * 5),
+			SHORT_TEXT_WIDGET_WIDTH, WIDGET_HEIGHT,
+			ScreenTexts.EMPTY, this.textRenderer
+		);
+		entityDataTextWidget.alignLeft();
+		entityDataTextWidget.setTooltip(getTextWidgetTooltip("entitydata", false));	
+
+		TextWidget connectedEntitiesTextWidget = new TextWidget(
+			leftEdge, topEdge + (WIDGET_SPACING * 6),
+			SHORT_TEXT_WIDGET_WIDTH, WIDGET_HEIGHT,
+			ScreenTexts.EMPTY, this.textRenderer
+		);
+		connectedEntitiesTextWidget.alignLeft();
+		connectedEntitiesTextWidget.setTooltip(getTextWidgetTooltip("connectedentitiesuuid", false));	
+		//#endregion
+
+
+		//add texts
+		addDrawable(maxConnectedEntitiesTextWidget);
+		addDrawable(spawnCountTextWidget);
+		addDrawable(requiredPlayerRangeTextWidget);
+		addDrawable(spawnRangeTextWidget);
+		addDrawable(entityDataTextWidget);
+		addDrawable(connectedEntitiesTextWidget);
+
+		//add textfields
 		addDrawableChild(maxConnectedEntitiesWidget);
 		addDrawableChild(spawnCountWidget);
 		addDrawableChild(requiredPlayerRangeWidget);
 		addDrawableChild(spawnRangeWidget);
 
+		//add buttons
 		addDrawableChild(entityDataClearWidget);
 		addDrawableChild(connectedEntitiesKillAllWidget);
-
 		addDrawableChild(doneButtonWidget);
 	}
 
@@ -143,14 +207,14 @@ public class MobRespawnerScreen extends Screen {
 		int topEdge = getTopEdge(height);
         context.drawTexture(TEXTURE, leftEdge - EDGE_SPACING, topEdge - EDGE_SPACING, 0, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
 
-		//draw all text
+		//draw all texts
 		context.drawText(this.textRenderer, title, getLeftEdge(width), getTopEdge(height), 4144959, false);
-		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.maxconnectedentities"), leftEdge, topEdge + 24, 4144959, false);
-		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.spawncount"), leftEdge, topEdge + 44, 4144959, false);
-		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.requiredplayerrange"), leftEdge, topEdge + 64, 4144959, false);
-		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.spawnrange"), leftEdge, topEdge + 84, 4144959, false);
-		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.entitydata"), leftEdge, topEdge + 104, 4144959, false);
-		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.connectedentitiesuuid"), leftEdge, topEdge + 124, 4144959, false);
+		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.maxconnectedentities"), leftEdge, topEdge + WIDGET_SPACING + 4, 4144959, false);
+		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.spawncount"), leftEdge, topEdge + (WIDGET_SPACING * 2) + 4, 4144959, false);
+		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.requiredplayerrange"), leftEdge, topEdge + (WIDGET_SPACING * 3) + 4, 4144959, false);
+		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.spawnrange"), leftEdge, topEdge + (WIDGET_SPACING * 4) + 4, 4144959, false);
+		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.entitydata"), leftEdge, topEdge + (WIDGET_SPACING * 5) + 4, 4144959, false);
+		context.drawText(this.textRenderer, Text.translatable("screen.respite_bench.mob_respawner.connectedentitiesuuid"), leftEdge, topEdge + (WIDGET_SPACING * 6) + 4, 4144959, false);
 
 		super.render(context, mouseX, mouseY, delta);
 	}
@@ -181,6 +245,15 @@ public class MobRespawnerScreen extends Screen {
 
 		}
 		super.close();
+	}
+
+	protected static Tooltip getTextWidgetTooltip(String key, boolean sameBehaviourAsVanillaSpawner) {
+		return Tooltip.of(
+			Text.translatable("screen.respite_bench.mob_respawner." + key).formatted(Formatting.AQUA)
+			.append(ScreenTexts.LINE_BREAK).append(ScreenTexts.LINE_BREAK).append(Text.translatable("screen.respite_bench.mob_respawner." + key + ".desc1").formatted(Formatting.WHITE))
+			.append(Text.translatable(sameBehaviourAsVanillaSpawner ? "screen.respite_bench.mob_respawner.sameasvanillaspawner" : "").formatted(Formatting.GRAY).formatted(Formatting.ITALIC))
+			.append(ScreenTexts.LINE_BREAK).append(ScreenTexts.LINE_BREAK).append(Text.translatable("screen.respite_bench.mob_respawner." + key + ".desc2").formatted(Formatting.DARK_GRAY))
+		);
 	}
 
 	protected static int getLeftEdge(int width) {
